1b518d64a894e14dce1080383d258b5bd6971965
Karel Nagel <nagelkarel@gmail.com>
2026-08-22 21:49:18 +0300

    Disable microphone capture on Asius hardware

--- openpilot/system/manager/process_config.py
+++ openpilot/system/manager/process_config.py
@@ -88,7 +88,7 @@ procs = [
   PythonProcess("webcamerad", "openpilot.system.camerad.webcam.camerad", driverview, enabled=WEBCAM),
   PythonProcess("proclogd", "openpilot.system.proclogd", only_onroad, enabled=platform.system() != "Darwin"),
   PythonProcess("journald", "openpilot.system.journald", only_onroad, platform.system() != "Darwin"),
-  PythonProcess("micd", "openpilot.system.micd", iscar),
+  PythonProcess("micd", "openpilot.system.micd", iscar, enabled=not ASIUS_HARDWARE),
   PythonProcess("timed", "openpilot.system.timed", always_run, enabled=not PC),
 
   PythonProcess("modeld", "openpilot.selfdrive.modeld.modeld", only_onroad),
